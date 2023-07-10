7a76592826ed3f47c5783b0f88d3edaf0c4a360a
Pavel Viarbitski <verbipo@gmail.com>
2023-07-10 14:25:30 +0200

    feat: simple helpers

---
+++ src/helpers/return-data.ts
@@ -0,0 +1,17 @@
+import { ServerResponse } from 'node:http';
+import User from 'interfaces';
+
+export default function returnData(
+  res: ServerResponse,
+  message: string | User | User[],
+  code: number,
+) {
+  res.statusCode = code;
+  res.setHeader('Content-Type', 'application/json');
+
+  res.end(
+    JSON.stringify(
+      typeof message === 'string' ? { message: message } : message,
+    ),
+  );
+}
---
+++ src/helpers/uuid-validater.ts
@@ -0,0 +1,6 @@
+export default function isValidUUID(uuid: string | undefined): boolean {
+  if (typeof uuid !== 'string') return false;
+  const reg =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return reg.test(uuid);
+}
