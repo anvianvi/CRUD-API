1f48b13721636214d92953d08849f20be0ad93e1
Pavel <verbipo@gmail.com>
2024-02-08 18:35:28 +0100

    refactor optimize post function

--- src/methods/post.ts
+++ src/methods/post.ts
@@ -2,6 +2,8 @@ import { IncomingMessage, ServerResponse } from 'node:http';
 import { v4 as uuidv4 } from 'uuid';
 import returnData from '../helpers/return-data';
 import { User } from 'interfaces';
+import { pipeline } from 'stream/promises';
+import { Writable } from 'stream';
 
 export default async function methodPost(
   url: string,
@@ -10,37 +12,35 @@ export default async function methodPost(
   users: User[],
 ) {
   if (url !== '/api/users') {
-    returnData(res, 'Invalid Data', 400);
-    return;
+    return returnData(res, `Invalid Data`, 400);
   }
 
   let requestBody = '';
 
-  await new Promise<void>((resolve) => {
-    req.on('data', (chunk) => {
-      requestBody += chunk.toString();
-    });
-
-    req.on('end', () => {
-      resolve();
-    });
-  });
+  await pipeline(
+    req,
+    new Writable({
+      write(chunk, _, callback) {
+        requestBody += chunk.toString();
+        callback();
+      },
+    }),
+  );
 
   const { username, age, hobbies } = JSON.parse(requestBody);
 
-  if (!username || !age) {
-    returnData(res, 'Missing required fields', 400);
-    return;
+  if (!username || !age || !hobbies) {
+    return returnData(res, `Missing required fields`, 400);
   }
 
   const newUser: User = {
     id: uuidv4(),
     username,
     age,
-    hobbies: hobbies || [],
+    hobbies: hobbies,
   };
 
   users.push(newUser);
 
-  returnData(res, newUser, 201);
+  return returnData(res, newUser, 201);
 }
