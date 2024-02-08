32e980210b71c4f37c5955b83a753fe41e05847b
Pavel <verbipo@gmail.com>
2024-02-08 18:24:40 +0100

    reafactor: optimize return data function

--- src/helpers/return-data.ts
+++ src/helpers/return-data.ts
@@ -6,11 +6,6 @@ export default function returnData(
   message: string | User | User[],
   code: number,
 ) {
-  const data = typeof message === 'string' ? { message } : message;
-
-  response.writeHead(code, {
-    'Content-Type': 'application/json',
-  });
-
-  response.end(JSON.stringify(data));
+  response.writeHead(code, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify({ message }));
 }
